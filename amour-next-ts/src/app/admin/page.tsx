5bbcbc9bea801abf16dcae17bc9f66ee5cafe6ef
Sushant <sushantkumaryadav912@gmail.com>
2025-07-24 23:36:59 +0530

    Formatted the file structure and added the basic templete to work upon

---
+++ amour-next-ts/src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+const adminPage: FC = () => {
+  return (
+    <div>
+      <h1>admin Page</h1>
+      {/* Add content here */}
+    </div>
+  );
+};
+
+export default adminPage;
